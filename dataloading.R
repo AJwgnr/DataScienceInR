c03f2cfbbdaad245174610d1bd0dc3c1e8434045
Alexander <alexws@live.de>
2018-11-10 14:53:08 +0100

    changed aims of proposal and changed the data loading

--- dataloading.R
+++ dataloading.R
@@ -2,35 +2,44 @@ library(readr)
 library(readxl)
 library(psych)
 
+
+csv_movement_folder_path <- folder <- "./res/position_data/" 
+csv_persons_folder_path <- ("./res/person_data/minecraft_all_subjects.xlsx")
+#lockBinding("csv_movement_folder_path", globalenv())
+#lockBinding("csv_persons_folder_path", globalenv())
+
+
+
 #Load datasets
-minecraft_all_subjects <- read_excel("./res/person_data/minecraft_all_subjects.xlsx")
 
-folder <- "./res/position_data/"      
-# path to folder that holds multiple .csv files
+  file_list <- list.files(path=csv_movement_folder_path, pattern="*.csv") 
+  # read in each .csv file in file_list and create a data frame with the same name as the .csv file
+  for (i in 1:length(file_list)){
+    assign(file_list[i], read.csv(paste(csv_movement_folder_path, file_list[i], sep='')))
+  }
+  persons <- read_excel(csv_persons_folder_path)
 
-file_list <- list.files(path=folder, pattern="*.csv") 
-# create list of all .csv files in folder
 
-# read in each .csv file in file_list and create a data frame with the same name as the .csv file
-for (i in 1:length(file_list)){
+
+
+viewData <- function(){
+  #View Dataset
+  View(persons)
   
-  assign(file_list[i], 
-         
-         read.csv(paste(folder, file_list[i], sep=''))
-         
-  )}
-
-#View Datasets
-View(minecraft_all_subjects)
-View(minecraft_pos_log_VP41_Tag1)
-
-#Preprocessing
-#TODO: Filter values or do NaN Processing
+  #Data summary
+  summary(persons)
+  describe(persons)
+  cor(persons)
+}
 
 
+filterData <- function(){
+  
+  #Preprocessing
+  #TODO: Filter values or do NaN Processing
+  
+}
 
-#Data summary
-summary(minecraft_all_subjects)
-describe(minecraft_all_subjects)
-cor(minecraft_all_subjects)
+
+
 
