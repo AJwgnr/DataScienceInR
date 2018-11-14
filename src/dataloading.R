412edbc5ca81ec2e3c5002b49b776669aa0a8cdf
Johannes Dambacher <johannes.dambacher@ovgu.de>
2018-11-14 14:18:54 +0100

    update gitignore

--- src/dataloading.R
+++ src/dataloading.R
@@ -3,21 +3,22 @@ library(readxl)
 library(psych)
 
 
-csv_movement_folder_path <- folder <- "./res/position_data/" 
-csv_persons_folder_path <- ("./res/person_data/minecraft_all_subjects.xlsx")
+csv_movement_folder_path <- folder <- "./../res/position_data/" 
+csv_persons_folder_path <- ("./../res/person_data/minecraft_all_subjects.xlsx")
 #lockBinding("csv_movement_folder_path", globalenv())
 #lockBinding("csv_persons_folder_path", globalenv())
 
 
 
 #Load datasets
+persons <- read_excel(csv_persons_folder_path)
 
-  file_list <- list.files(path=csv_movement_folder_path, pattern="*.csv") 
-  # read in each .csv file in file_list and create a data frame with the same name as the .csv file
-  for (i in 1:length(file_list)){
-    assign(file_list[i], read.csv(paste(csv_movement_folder_path, file_list[i], sep='')))
-  }
-  persons <- read_excel(csv_persons_folder_path)
+file_list <- list.files(path=csv_movement_folder_path, pattern="*.csv") 
+# read in each .csv file in file_list and create a data frame with the same name as the .csv file
+for (i in 1:length(file_list)){
+  assign(file_list[i], read.csv(paste(csv_movement_folder_path, file_list[i], sep='')))
+}
+ 
 
 
 
