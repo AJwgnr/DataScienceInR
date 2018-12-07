398b8ab1ca87c8a5c1e74312278ad31bbe9e502a
Johannes Dambacher <johannes.dambacher@ovgu.de>
2018-12-07 12:03:23 +0100

    cerate tabs for ui.R remove example in server.R

--- src/MineR/ui.R
+++ src/MineR/ui.R
@@ -3,31 +3,93 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
+# see https://rstudio.github.io/shinydashboard/structure.html
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
+library(shiny)
+library(shinydashboard)
+
+ds_header = dashboardHeader(title = "MineR")
+
+ds_sidebar = dashboardSidebar(
+  sidebarMenu(
+    id = "menuTasks",
+    menuItem(
+      "Introduction",
+      tabName = "introduction",
+      icon = icon("th")),
+    menuItem(
+      "The Data - raw",
+      tabName = "rawData",
+      icon = icon("th")),
+    menuItem(
+      "Visual data exploration",
+      tabName = "visualization",
+      icon = icon("th")
     ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+    menuItem(
+      "Trajectory feature exploration",
+      tabName = "trjFeatures",
+      icon = icon("th")
+    ),
+    menuItem(
+      "Clustering",
+      tabName = "clustering",
+      icon = icon("th")),
+    menuItem(
+      "Decision Trees",
+      tabName = "decisionTree",
+      icon = icon("th"))
   )
-))
+)
+
+ds_body = dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "introduction",
+        h2("Introduction"),
+        fluidRow(
+          tabBox(
+            title = "First tabBox",
+            # The id lets us use input$tabset1 on the server to find the current tab
+            id = "tabset1", height = "250px",
+            tabPanel("Tab1", "First tab content"),
+            tabPanel("Tab2", "Tab content 2")
+          ),
+          tabBox(
+            side = "right", height = "250px",
+            selected = "Tab3",
+            tabPanel("Tab1", "Tab content 1"),
+            tabPanel("Tab2", "Tab content 2"),
+            tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "rawData",
+        h2("The data - raw")
+      ),
+      tabItem(
+        tabName = "visualization",
+        h2("Visual data exploration")
+      ),
+      tabItem(
+        tabName = "trjFeatures",
+        h2("Trajectory feature exploration")
+      ),
+      tabItem(
+        tabName = "clustering",
+        h2("Clustering")
+      ),
+      tabItem(
+        tabName = "decisionTree",
+        h2("Decision tree")
+      )
+    )
+)
+
+# Define UI for application that draws everything
+dashboardPage(ds_header, ds_sidebar, ds_body)
